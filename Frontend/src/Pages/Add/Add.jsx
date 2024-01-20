4c545a029040b2f66d28973f8249973c622f5943
nahjd <nahidmemmedov711@gmail.com>
2024-01-20 17:33:32 +0400

    asdasd

--- Frontend/src/Pages/Add/Add.jsx
+++ Frontend/src/Pages/Add/Add.jsx
@@ -7,10 +7,13 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import React, { useEffect } from "react"
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import React, { useEffect, useState } from "react"
 import * as Yup from 'yup';
+import { Helmet } from "react-helmet";
 import { fetchData, fetchDelete, fetchPost } from '../../redux/Slice/userSlice';
-
+import "./Add.scss"
 
 export const SignupSchema = Yup.object().shape({
     name: Yup.string()
@@ -28,14 +31,68 @@ export const SignupSchema = Yup.object().shape({
 const Add = () => {
     const dispatch = useDispatch()
     const data = useSelector((state) => state.meal.data)
-
+    const [search, setSearch] = useState("")
+    const [type, setType] = useState("")
     useEffect(() => {
         dispatch(fetchData())
     }, [dispatch])
     console.log(data);
+    const filteredData = () => {
+        if (type == "az") {
+            return [...data].sort((a, b) => a.name.localeCompare(b.name))
+        }
+        if (type == "za") {
+            return [...data].sort((a, b) => b.name.localeCompare(a.name))
+        }
+        else if (type == "price") {
+            return [...data].sort((a, b) => Number(a.price) - Number(b.price))
+        }
+        else {
+            return data
+        }
+
+    }
+
+
     return (
         <>
-            Add
+
+            <div className="table">
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>Add</title>
+                    <link rel="canonical" href="http://mysite.com/example" />
+                </Helmet>
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" onClick={(e) => {
+                    setSearch(e.target.value)
+                }} />
+                <Button
+                    onClick={() => {
+                        setType("az")
+                    }}
+
+                    variant="outlined">A-Z</Button>
+                <Button
+                    onClick={() => {
+                        setType("za")
+                    }}
+
+                    variant="outlined">Z-A</Button>
+                <Button
+                    onClick={() => {
+                        setType("price")
+                    }}
+
+                    variant="outlined">Price</Button>
+
+                <Button
+                    onClick={() => {
+                        setType("")
+                    }}
+
+                    variant="outlined">Default</Button>
+            </div>
+
             <Formik
                 initialValues={{
                     name: '',
@@ -76,7 +133,8 @@ const Add = () => {
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {data.map((row) => (
+                        {filteredData().filter(item => item.name.toLowerCase().includes(search.toLowerCase())).map((row) => (
+
                             <TableRow
                                 key={row._id}
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -86,6 +144,7 @@ const Add = () => {
                                 </TableCell>
                                 <TableCell align="right">{row.about}</TableCell>
                                 <TableCell align="right">{row.image}</TableCell>
+                                <TableCell align="right">{row.price}</TableCell>
 
                                 <TableCell style={{ cursor: "pointer" }}
                                     onClick={() => {
@@ -93,7 +152,9 @@ const Add = () => {
                                     }}
                                     align="right">Delete</TableCell>
                             </TableRow>
+
                         ))}
+
                     </TableBody>
                 </Table>
             </TableContainer>
